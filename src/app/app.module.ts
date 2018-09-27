cdaf5c6e985e0627ef38c17fba067c2ea9c3aae7
Chaithraprakash <chaithra.prakash@gmail.com>
2018-09-27 23:30:31 +0530

    Login Changes
    Login Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,31 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {RouterModule, Routes}  from '@angular/router'; 
+
+const appRoutes : Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path : 'dashboard',
+    component : DashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
+
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AngularFontAwesomeModule
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  loginUser(e){
+    e.preventDefault();
+    console.log(e);
+var username = e.target.elements[0].value;
+var password = e.target.elements[1].value;
+if(username == "admin" && password == "password"){
+this.router.navigate(['dashboard']);
+}
   }
 
 }
